8746b606da5d1c917bcf1b8db82766033aefe66b
spyking <raghv.indr@gmail.com>
2017-02-06 21:49:24 +0530

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // connect to the local mongodb database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // connect to the local mongodb database or the heroku database
 
 module.exports = {mongoose};
