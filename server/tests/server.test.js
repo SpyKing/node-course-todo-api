4fbc04cc8b90cdea875b9169608e4fac8fb3458d
spyking <raghv.indr@gmail.com>
2017-02-07 19:44:34 +0530

    Add tests for DELETE /todos/:id  route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,7 @@ const todos = [{
     _id: new ObjectID(),
     text: 'First test todo'
 }, {
-    __id: new ObjectID(),
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -97,6 +97,45 @@ describe('GET /todos/:id', () => {
     it('should return a 404 for non-object ids', (done) => {
         request(app)
         .get('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var id = todos[1]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(id);
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.findById(id).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((err) => done(err));
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectID().toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+         request(app)
+        .delete('/todos/123')
         .expect(404)
         .end(done);
     });
