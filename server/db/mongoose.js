9efaf6c9700677e64588c8ff94bf77ea34b8a7b2
spyking <raghv.indr@gmail.com>
2017-02-08 21:35:33 +0530

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // connect to the local mongodb database or the heroku database
+// connect to the local mongodb database or the heroku database
+mongoose.connect(process.env.MONGODB_URI); 
 
 module.exports = {mongoose};
