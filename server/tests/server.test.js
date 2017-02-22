af5c0d2eff7d9f7018dd4e7704e0850eb4c8ac5b
spyking <raghv.indr@gmail.com>
2017-02-22 11:06:03 +0530

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -218,8 +218,8 @@ describe('POST /users', () => {
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
                 done();
-            });
-        }).catch((e) => done());
+            }).catch((e) => done());;
+        });
     });
 
     it('should return validation errors if request is invalid', (done) => {
@@ -291,6 +291,25 @@ describe('POST /users/login', () => {
                 expect(user.tokens.length).toBe(0);
                 done();
             }).catch((e) => done());
+        });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on log out', (done) => {
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((e) => {
+            if (e) {
+                return done(e);
+            }
+
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
         });
     });
 });
